bb168e793748dfa8728048b6294c16cafe935997
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-20 20:41:02 +0530

    added files

---
+++ src/components/Cards.js
@@ -0,0 +1,18 @@
+function Cards(){
+    return <section id= "cards__container" className="p-4 grid grid-cols-4 gap-x-4 h-48">
+        <div className="w-full h-full bg-gradient-to-r from-medium-blue to-light-blue rounded-lg">
+
+        </div>
+        <div className="w-full h-full bg-darker-blue">
+
+        </div>
+        <div className="w-full h-full bg-darker-blue">
+
+        </div>
+        <div className="w-full h-full bg-darker-blue">
+
+        </div>
+    </section>
+}
+
+export default Cards
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import Header from "./Header";
+import Cards from "./Cards";
 function Main(){
     return (
         <section id="main" className="min-h-screen bg-darkest-blue">
             <Header/>
+            <Cards/>
         </section>
     )
 }
