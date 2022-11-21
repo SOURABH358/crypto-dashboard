6c24683c235d2556e6b7f454eb7c4a99bf2189e7
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-21 12:02:00 +0530

    added files

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,8 +1,15 @@
 function SideBar(){
     return(
-        <section id = "side__bar" className="min-h-screen bg-darker-blue">
-            <div className=""></div>
-            <ul className="">
+        <section id = "side__bar" className="min-h-screen bg-darker-blue py-4 px-2">
+            <div id ="logo" className="text-medium-blue px-4 text-2xl font-bold">COINF</div>
+            <ul id = "menu__bar" className="list-none flex flex-col gap-6 py-12 text-grey pl-4">
+                <li>Overview</li>
+                <li>Trader Profile</li>
+                <li>Echarts</li>
+                <li>Mailbox</li>
+                <li>Pages</li>
+            </ul>
+            <ul id = "others" className="">
 
             </ul>
         </section>
