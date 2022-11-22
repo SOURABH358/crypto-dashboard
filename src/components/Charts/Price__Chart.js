d255035340ccfdefba6074f50d57bf989e56de70
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-22 22:18:23 +0530

    added files

--- src/components/Charts/Price__Chart.js
+++ src/components/Charts/Price__Chart.js
@@ -26,10 +26,21 @@ import {
     smooth:true,
     lineTension: 0.3,
     radius:0,
-    fill:true,
+    scales: {
+        xAxes: [{
+            gridLines: {
+                display:false
+            }
+        }],
+        yAxes: [{
+            gridLines: {
+                display:false
+            }   
+        }]
+    },
     plugins: {
       legend: {
-        position: 'top',
+        display: false
       },
       
     },
@@ -42,29 +53,14 @@ function Price__chart() {
         datasets: [{
             label: 'My First Dataset',
             data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
-              ],
+            backgroundColor: 'rgba(54, 162, 235,0.4)',
               borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
                 'rgb(54, 162, 235)',
                 'rgb(153, 102, 255)',
                 'rgb(201, 203, 207)'
               ],
-              borderWidth: 1,
-              fill: {
-                target: 'origin',
-                above: 'rgb(255, 0, 0)',   
-              }
+              borderWidth: 2,
+              fill: true
         }]
     };
     return (
